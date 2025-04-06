ca2ce05d5ff6a3c57d014a081f4e79f1a12be9ff
Alireza <mahmoudian@computer-4.station>
2025-04-06 23:04:23 +0200

    Switched gsheet authorize function argument

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from validation import validate_email, validate_codice_fiscale, duration_overlap
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Env Vars
-gc = pygsheets.authorize(service_file=os.environ['GSHEETS'])    
+gc = pygsheets.authorize(service_account_json=os.environ['GSHEETS'])    
 wks = gc.open('Biblio-logs').worksheet_by_title('logs')
 
 load_dotenv()
