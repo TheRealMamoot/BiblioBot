1bd4e06be565acf01c283199b27f5c611884534e
Alireza Mahmoudian <alireza.mahmoudian.am@gmail.com>
2025-05-21 15:17:29 +0200

    fix(db): test without biuld_db

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import logging
 from telegram.ext import Application
 
 from src.biblio.app import build_app
-from src.biblio.db.build import build_db
 from src.biblio.utils.parser import get_parser
 
 
@@ -12,7 +11,6 @@ async def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     parser = get_parser()
     args = parser.parse_args()
-    await build_db(db_env=args.db_env)
     app: Application = build_app(token_env=args.token_env, priorities_env=args.priorities_env, db_env=args.db_env)
     await app.initialize()
     await app.start()
